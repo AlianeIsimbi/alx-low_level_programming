203a172989dd4ec7109244a463019f9008c743a7
AlianeIsimbi <isimbialiane@gmail.com>
2022-12-17 16:30:20 -0800

    More functions, nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_number prints an integer.
+ * print_number -  prints an integer.
  * @n: input integer.
  * Return: no return.
  */
@@ -29,4 +29,4 @@ void print_number(int n)
 	{
 		_putchar(((m / count) % 10) + 48);
 	}
-}	
+}
